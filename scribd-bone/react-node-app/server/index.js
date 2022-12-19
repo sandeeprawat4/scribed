505bec4304dce6a454fb7d2e6ed2928c03dcd838
sandeep rawat <sandeep.rawat4@gmail.com>
2022-12-19 23:41:38 +0530

    First commit

---
+++ scribd-bone/react-node-app/server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('/api', (req, res) => {
+    res.json({message: "Hello from server!"});
+});
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
