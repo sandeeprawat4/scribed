ab944455fb3bbb0b9401437a5119ffc47767f6b5
sandeep rawat <sandeep.rawat4@gmail.com>
2022-12-22 23:19:21 +0530

    login changes

--- scribd-face/client/src/App.js
+++ scribd-face/client/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import Login from './Login';
+import "./App.css" ;
 
 function App() {
-  const[data, setData] = React.useState(null);
-  React.useEffect(() => {
+  //const[data, setData] = React.useState(null);
+  /*React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data)=>setData(data.message));
-  }, []);
+  }, []);*/
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data? "Loading..." : data}
-        </p>
-      </header>
+      <div>
+        <Login/>
+      </div>
     </div>
   );
 }
