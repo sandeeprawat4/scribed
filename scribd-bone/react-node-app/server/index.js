45da1db55232b7d66ad1e38eafa2943b398da762
sandeep rawat <sandeep.rawat4@gmail.com>
2023-01-05 18:23:34 +0530

    Integrated Google SAML

--- scribd-bone/react-node-app/server/index.js
+++ scribd-bone/react-node-app/server/index.js
@@ -1,15 +1,111 @@
 const express = require("express");
-const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
+require("dotenv/config");
+const cors = require("cors");
+const {OAuth2Client} = require("google-auth-library");
+const jwt = require("jsonwebtoken");
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.get('/api', (req, res) => {
-    res.json({message: "Hello from server!"});
-});
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+
+app.use(
+  cors({
+      origin: ["http://localhost:3000"],
+      methods: "GET,POST,PUT,DELETE,OPTIONS"
+  })
+);
+app.use(express.json());
+
+let DB=[];
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+async function verifyGoogleToken(token) {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    return { payload: ticket.getPayload() };
+  } catch (error) {
+    return { error: "Invalid user detected. Please try again" };
+  }
+}
+
+app.post("/signup", async (req, res) => {
+    try {
+      // console.log({ verified: verifyGoogleToken(req.body.credential) });
+      if (req.body.credential) {
+        const verificationResponse = await verifyGoogleToken(req.body.credential);
+  
+        if (verificationResponse.error) {
+          return res.status(400).json({
+            message: verificationResponse.error,
+          });
+        }
+  
+        const profile = verificationResponse?.payload;
+  
+        DB.push(profile);
+  
+        res.status(201).json({
+          message: "Signup was successful",
+          user: {
+            firstName: profile?.given_name,
+            lastName: profile?.family_name,
+            picture: profile?.picture,
+            email: profile?.email,
+            token: jwt.sign({ email: profile?.email }, "mySecret", {
+              expiresIn: "1d",
+            }),
+          },
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "An error occurred. Registration failed.",
+      });
+    }
+  });
+
+  app.post("/login", async (req, res) => {
+    try {
+      if (req.body.credential) {
+        const verificationResponse = await verifyGoogleToken(req.body.credential);
+        if (verificationResponse.error) {
+          return res.status(400).json({
+            message: verificationResponse.error,
+          });
+        }
+  
+        const profile = verificationResponse?.payload;
+  
+        const existsInDB = DB.find((person) => person?.email === profile?.email);
+  
+        if (!existsInDB) {
+          return res.status(400).json({
+            message: "You are not registered. Please sign up",
+          });
+        }
+  
+        res.status(201).json({
+          message: "Login was successful",
+          user: {
+            firstName: profile?.given_name,
+            lastName: profile?.family_name,
+            picture: profile?.picture,
+            email: profile?.email,
+            token: jwt.sign({ email: profile?.email }, process.env.JWT_SECRET, {
+              expiresIn: "1d",
+            }),
+          },
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error?.message || error,
+      });
+    }
+  });
+
+
+
+app.listen("5152", () => console.log("server running on the port 5152"));
--- scribd-face/client/src/App.js
+++ scribd-face/client/src/App.js
@@ -1,21 +1,39 @@
 import React from "react";
-import Login from './Login';
-import "./App.css" ;
+import {useState, useEffect} from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home, Landing, Login, Signup } from "./screens";
 
-function App() {
-  //const[data, setData] = React.useState(null);
-  /*React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data)=>setData(data.message));
-  }, []);*/
-  return (
-    <div className="App">
-      <div>
-        <Login/>
-      </div>
-    </div>
+const App=() => {
+  useEffect(() => {
+    const theUser = localStorage.getItem("user");
+
+    if (theUser && !theUser.includes("undefined")) {
+      setUser(JSON.parse(theUser));
+    }
+  }, []);
+  const[user, setUser] = useState({});
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={user?.email ? <Navigate to="/home" /> : <Landing />}
+        />
+        <Route
+          path="/signup"
+          element={user?.email ? <Navigate to="/home" /> : <Signup />}
+        />
+        <Route
+          path="/login"
+          element={user?.email ? <Navigate to="/home" /> : <Login />}
+        />
+        <Route
+          path="/home"
+          element={user?.email ? <Home user={user} /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
